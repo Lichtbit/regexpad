c2d7045a2916e37ada67609b67446115fe5e4930
georf <georf@georf.de>
2012-10-09 00:36:53 +0200

    add jsdoc lib for generating doc

---
+++ tools/jsdoc-toolkit/run-doc-compiler.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+java -jar jsrun.jar app/run.js \
+../../js/parser/RegularExpression.js \
+../../js/parser/Javascript.js \
+../../js/parser/Ajax.js \
+../../js/tour/Help.js \
+../../js/tour/HighlightStep.js \
+../../js/tour/LowlightStep.js \
+../../js/tour/MoveMessageStep.js \
+../../js/tour/SetMessageStep.js \
+../../js/tour/Step.js \
+../../js/tour/WaitStep.js \
+../../js/tour/TypeInto.js \
+../../js/Flag.js \
+../../js/UIMatchText.js \
+../../js/MatchText.js \
+../../js/UIMessageService.js \
+../../js/UICodeSnip.js \
+../../js/Permalink.js \
+../../js/config.js \
+../../js/RegularExpressionCache.js \
+../../js/RegHex.js \
+../../js/ui.js \
+-t=templates/jsdoc/
+
