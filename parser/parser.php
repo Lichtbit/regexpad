bae1531eb7b304038e1d5aab0ced368992bdf3fc
Georg Limbach <georf@georf.de>
2013-04-24 21:13:15 +0200

    delete temp files

--- parser/parser.php
+++ parser/parser.php
@@ -3,45 +3,50 @@ error_reporting(E_ALL);
 
 
 if (isset($_POST['json'])) {
-	$input = json_decode($_POST['json']);
+    $input = json_decode($_POST['json']);
 
-	$vz = opendir(__DIR__);
-	while ($dir = readdir($vz)) {
-		if ($dir == $input->parser && is_dir(__DIR__.'/'.$dir) && is_file(__DIR__.'/'.$dir.'/class.php')) {
-			include_once(__DIR__.'/'.$dir.'/class.php');
-			break;
-		}
-	}
-	closedir($vz);
+    $vz = opendir(__DIR__);
+    while ($dir = readdir($vz)) {
+        if ($dir == $input->parser && is_dir(__DIR__.'/'.$dir) && is_file(__DIR__.'/'.$dir.'/class.php')) {
+            include_once(__DIR__.'/'.$dir.'/class.php');
+            break;
+        }
+    }
+    closedir($vz);
 
-	if (!class_exists('Parser') && !class_exists('InternParser')) {
-		$input->error = 'Parser not exists';
-		echo json_encode($input);
-		exit();
-	}
-
-
-	if (class_exists('Parser')) {
+    if (!class_exists('Parser') && !class_exists('InternParser')) {
+        $input->error = 'Parser not exists';
+        echo json_encode($input);
+        exit();
+    }
 
 
-		$tmpFilename = getTmpPath();
-		file_put_contents($tmpFilename, $_POST['json']);
+    if (class_exists('Parser')) {
 
-		$cmd = Parser::getCmdLine();
-		passthru($cmd .' < '.$tmpFilename);
-		exit();
-	} elseif (class_exists('InternParser')) {
-		InternParser::run($_POST['json']);
-		exit();
-	}
+
+        // create temp file
+        $tmpFilename = getTmpPath();
+        file_put_contents($tmpFilename, $_POST['json']);
+
+        $cmd = Parser::getCmdLine();
+        passthru($cmd .' < '.$tmpFilename);
+
+        // remove temp file
+        unlink($tmpFilename);
+        exit();
+
+    } elseif (class_exists('InternParser')) {
+        InternParser::run($_POST['json']);
+        exit();
+    }
 }
 
 function getTmpPath() {
-	$tmpFilename = '/tmp/parserregexp';
-	do {
-		$hash = md5(rand());
-	} while(is_file($tmpFilename.$hash));
+    $tmpFilename = '/tmp/parserregexp';
+    do {
+        $hash = md5(rand());
+    } while(is_file($tmpFilename.$hash));
 
-	$tmpFilename .= $hash;
-	return $tmpFilename;
+    $tmpFilename .= $hash;
+    return $tmpFilename;
 }
